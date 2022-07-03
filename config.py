8abf62521ea9a9b6b47f38978cf087ea71bac954
flavien-hugs <geekactux@gmail.com>
2022-07-03 18:54:12 +0000

    edit database

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = "sqlite://" + os.path.join(BASE_DIR, 'app.db')
