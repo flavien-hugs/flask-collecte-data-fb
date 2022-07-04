83aa0bbe3806f327acd233bc514ceae0dc078761
flavien-hugs <geekactux@gmail.com>
2022-07-04 19:24:29 +0000

    edit utils function

---
+++ apps/utils.py
@@ -0,0 +1,8 @@
+import random
+
+from fbapp.models import Content, Gender
+
+
+def find_content(gender):
+    contents = Content.query.filter(Content.gender == Gender[gender]).all()
+    return random.choice(contents)
