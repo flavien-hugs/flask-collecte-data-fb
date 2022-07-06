b510a57c1265dd82b26482edd0c30152b76c2dfb
flavien-hugs <geekactux@gmail.com>
2022-07-06 20:18:41 +0000

    improve configuration

--- config.py
+++ config.py
@@ -5,12 +5,12 @@
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
-
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 if os.environ.get('DATABASE_URL') is None:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'app.db')
+    FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+    FACEBOOK_APP_ID = '349810240658976'
